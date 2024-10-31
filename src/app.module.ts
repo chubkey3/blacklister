f9bfb45614f22f0c843bb1cda0cd72cd1a29be23
Chubkey <69558928+Ravager6969@users.noreply.github.com>
2024-10-31 02:09:17 -0700

    added postgresql integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// database fields
+const port = parseInt(process.env.PORT || "5432")
+const username = process.env.USERNAME || ""
+const password = process.env.PASSWORD || ""
+const database = process.env.DATABASE || ""
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: port,
+      username: username,
+      password: password,
+      database: database,
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
