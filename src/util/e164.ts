b1edbbc2d2f81e24bec664a6f920ce1f207a72da
Chubkey <69558928+Ravager6969@users.noreply.github.com>
2024-10-31 03:45:52 -0700

    feat: added E.164 helper w/ tests

---
+++ src/util/e164.ts
@@ -0,0 +1,25 @@
+/**
+ * Converts phone number string to E.164 format
+ * Note: applies to US-based phone numbers only
+ * @param {string} phoneNumber The phone number to be converted
+ * @return {string} The result of converting phoneNumber to E.164
+ */
+export function toE164(phoneNumber: string) {
+
+    // already in E.164 format
+    if (phoneNumber.startsWith('+')) {
+        return phoneNumber;
+    }
+
+    // parse phone number into just number (no spaces, dashes, or parenthesis)
+    let phoneNumberRaw = phoneNumber.replaceAll(/[ \-\(\)\.]/g, '');
+    
+    // if no country code
+    if (phoneNumberRaw.length == 10) {
+        return `+1${phoneNumberRaw}`
+    } else {
+        return `+${phoneNumberRaw}`
+    }
+}
+
+// test ASAP
